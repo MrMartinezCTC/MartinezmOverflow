005fcbf33a7e5ecadf1847f03e9d49e79607a878
DiPiazza-John-Student <112406340+jodi229@users.noreply.github.com>
2023-03-24 14:19:13 +0000

    login endpoint made

--- controllers/users.js
+++ controllers/users.js
@@ -1,29 +1,63 @@
 const express = require('express');
+const bcrypt = require ('bcrypt');
 const { User, validateUser } = require('../models/User');
 const router = express.Router();
 
 router.post('/signup', async (req, res) => {
+    let failMsg = '';
+    
     // First Validate The Request
     const { error } = validateUser(req.body);
-    if (error) {
-        return res.status(400).json(error.details[0].message);
+    if (error) failMsg = error.details[0].message;
+    else {
+        // Check if this user already exists
+        const user = await User.findOne({ email: req.body.email });
+        if (user) failMsg = 'The email provided is already in use.'
     }
 
-    // Check if this user already exists
-    let user = await User.findOne({ email: req.body.email });
-    if (user) {
-        return res.status(400).json('That user already exists!');
-    } else {
-        // Insert the new user if they do not exist yet
-        user = new User({
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            email: req.body.email,
-            password: req.body.password
-        });
-        await user.save();
-        res.send(user);
+    if (failMsg) return res.status(400).json({
+        isError: true,
+        msg: failMsg
+    });
+
+    const hash = await bcrypt.hash(req.body.password, 10);
+    
+    const user = new User({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: hash
+    });
+    await user.save();
+    res.status(201).json(user);
+});
+
+
+router.post('/login', async (req, res) => {
+    let failure = false;
+    console.log(req.body);
+
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) failure = true;
+    else {
+        const passIsCorrect = await bcrypt.compare(req.body.password, user.password);
+
+        console.log(passIsCorrect);
+
+        if (!passIsCorrect) failure = true;
+
+        // failure = !await bcrypt.compare(req.body.password, user.password);
     }
+
+    if (failure) return res.status(400).json({
+        isError: true,
+        msg: 'Invalid email/password'
+    });
+
+
+    res.status(200).json({ success: true });
 });
 
 module.exports = router;
+
+
