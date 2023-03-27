83b09e43e41121711ede6de93d165fa2972a8927
DiPiazza-John-Student <112406340+jodi229@users.noreply.github.com>
2023-03-27 12:54:34 +0000

    rendering engine added. home page set up.

--- server.js
+++ server.js
@@ -11,12 +11,45 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 
+app.set("view engine", "ejs");
+
+app.get('/', (req, res) => {
+	const topQuestions = [
+		{
+			title: 'Pain. Suffering. Agony.',
+			problem: `
+				AAAAAAAGGHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH!!!!!!!!!!!!!!!!!!!!!
+				My code does not work even though I told it to. I hate this. Computers are stupid. We need to get back to the basics. People should not live this way.
+			`,
+			usefulness: 300,
+			answers: 4
+		},
+		{
+			title: 'How to create an array without knowing the length in C#?',
+			problem: `
+			I need to dynamically create an array where I won't know its length and stuff you know? It would just be so great to be able to do that. Like it would just be incedibly wodnerful. I would have unlimited power and it would just be great. I have tried, but c# just doesnt let me. It is limiting me. Help me! I demand it.
+			`,
+			usefulness: 30,
+			answers: 3
+		},
+		{
+			title: 'Pain. Suffering. Agony.',
+			problem: `
+				AAAAAAAGGHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH!!!!!!!!!!!!!!!!!!!!!
+				My code does not work even though I told it to. I hate this. Computers are stupid. We need to get back to the basics. People should not live this way.
+			`,
+			usefulness: 12,
+			answers: 2
+		},
+	]
+	res.render('index', {
+		questions: topQuestions
+	});
+});
+
 app.use(express.static('webapp'));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-	res.sendFile(`${__dirname}/webapp/index.html`);
-});
 
 app.use('/user', user);
 app.use('/question', question);
