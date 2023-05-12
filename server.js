0c73f338204b8615fb25537817ffab089c68f394
MrMartinezCTC <106337782+MrMartinezCTC@users.noreply.github.com>
2023-05-12 00:40:20 +0000

    Super farfetched idea and if it works imma be mad i've been at this all day i blame hunter

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const MONGOURI = `mongodb+srv://${process.env.MONGOSTRING}?retryWrites=true&w=majority`;
+const MONGOURI = `mongodb+srv://jodi229:c4B8xhva@cluster0.zkfhtus.mongodb.net/martinezMoverflow?retryWrites=true&w=majority`;
 
 try {
     await mongoose.connect(MONGOURI, {
@@ -16,7 +16,7 @@ try {
     throw e;
 }
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 const HOST = 'localhost';
 
 //app.listen(PORT, HOST);
