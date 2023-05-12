3ba04c31d83d36549b95515d1c92757f7d830d0f
DiPiazza-John-Student <112406340+jodi229@users.noreply.github.com>
2023-05-12 13:15:49 +0000

    moved await statement to function

--- server.js
+++ server.js
@@ -9,20 +9,23 @@ dotenv.config();
 
 const MONGOURI = `mongodb+srv://jodi229:c4B8xhva@cluster0.zkfhtus.mongodb.net/martinezMoverflow?retryWrites=true&w=majority`;
 
-try {
-    await mongoose.connect(MONGOURI, {
-        useNewUrlParser: true
-    });
-    console.log("Connected to DB!!");
-} catch (e) {
-    console.log(e);
-    throw e;
+async function startApp () {
+    try {
+        await mongoose.connect(MONGOURI, {
+            useNewUrlParser: true
+        });
+        console.log("Connected to DB!!");
+    } catch (e) {
+        console.log(e);
+        throw e;
+    }
+    const PORT = 8080;
+    const HOST = 'localhost';
+    
+    //app.listen(PORT, HOST);
+    app.listen(PORT);
+    
+    console.log(`Running on http://${HOST}:${PORT}`);
 }
 
-const PORT = 8080;
-const HOST = 'localhost';
-
-//app.listen(PORT, HOST);
-app.listen(PORT);
-
-console.log(`Running on http://${HOST}:${PORT}`);
+startApp();
